8cae3d6309be209aadc842ce1053eaf9bcd8495f
Ryc1x <jerry_huangrui@outlook.com>
2020-04-01 15:42:25 -0400

    added vue material

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+/* Imports from vue-material */
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
 
@@ -8,3 +12,7 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <md-button class="md-raised">Default</md-button>
+      <md-button class="md-raised" :md-ripple="false">Ripple Off</md-button>
+      <md-button class="md-raised md-primary">Primary</md-button>
+      <md-button class="md-raised md-accent">Accent</md-button>
+      <md-button class="md-raised" disabled>Disabled</md-button>
+    </div>
   </div>
 </template>
