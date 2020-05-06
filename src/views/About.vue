34ba576c55ccb44ac96e4551ddecf5ef20592dd9
akash <akashshaji321@gmail.com>
2020-05-05 22:23:56 -0400

    wrote about us page, probably needs a little more work

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,62 @@
 <template>
   <div class="about">
-    <h1>The Reason</h1>
+    <h1>About the Project</h1>
     <p>
-      While humanity at large has made great strides in various fields such as medicine, education and life expectancy, this progress is not without great cost to our world.
-      We need to be taking care of the world for future generations, and as such it's vital that we educate young people on topics relating to sustainability because, as a WPI professor explains, “[they] are going to be the ones to deal with it.”
+      While humanity at large has made great strides in various fields such as medicine, education and life expectancy,
+      this progress is not without great cost to our world.
+      We need to be taking care of the world for future generations, and as such it's vital that we educate young people
+      on topics relating to sustainability because, as a WPI professor explains, “[they] are going to be the ones to
+      deal with it.”
     </p>
     <p>
       WPI first years are expected to make a 4-year plan that maps their schedules to a set of graduation requirements.
       Unfortunately, while designing these plans, learning about sustainability is not a factor in their selections.
       The Climate Crisis, increased poverty and inequalities emphasize how essential learning about sustainability is.
-      The Association for the Advancement of Sustainability in Higher Education explains that “the classroom (is) the true battleground for change,” but WPI students do not realize how many courses cover sustainability, and some WPI faculty lack the resources to integrate sustainability into their curricula (“Ten Ways to Integrate Sustainability into the Curriculum,” 2013).
+      The Association for the Advancement of Sustainability in Higher Education explains that “the classroom (is) the
+      true battleground for change,” but WPI students do not realize how many courses cover sustainability, and some
+      WPI faculty lack the resources to integrate sustainability into their curricula.
     </p>
     <p>
       Our project aims to promote sustainability in education for WPI faculty and students.
-      We interviewed faculty who already teach sustainability themed curriculum and surveyed their students to find out what sustainability topics are currently being covered at WPI and what topics students want to learn about.
-      We used this information to generate our deliverable: a website that serves both students and faculty. It allows for students to filter through sustainable courses by subject or sustainability topic.
-      It allows faculty to have easy access to ways sustainability can be integrated in curriculum, potential help for doing so, and the student interest in sustainability topics and delivery methods.
+      We interviewed faculty who already teach sustainability themed curriculum and surveyed their students to find out
+      what sustainability topics are currently being covered at WPI and what topics students want to learn about.
+      We used this information to generate our deliverable: a website that serves both students and faculty. It allows
+      for students to filter through sustainable courses by subject or sustainability topic.
+      It allows faculty to have easy access to ways sustainability can be integrated in curriculum, potential help for
+      doing so, and the student interest in sustainability topics and delivery methods.
     </p>
-    <p>To read more about our project visit [here] (link will be inserted once our paper is published)</p>
+    <p>To read more about our project visit see (link will be inserted once our paper is published).</p>
+
+    <h1>About the Sustainable Development Goals</h1>
+    <p>
+      We chose to categorize the sustainability themed courses further according to the United Nations’ Sustainable
+      Development Goals.
+      The Sustainable Development Goals are 17 goals that were announced as part of the 2030 Agenda for Sustainable Development.
+      We chose to use these goals as categories for mapping sustainability because these goals encompass a wide array of
+      topics.
+      We believe that each sustainability themed course should relate in some way to one or more SDG.
+      WPI's Office of Sustainability has also expressed interest in promoting the Sustainable Development Goals in curricula.
+    </p>
+    <p>
+      To read more about the Sustainable Development Goals, visit the
+      <a href ="https://sustainabledevelopment.un.org/">Sustainable Development Goals Knowledge Platform.</a>
+    </p>
 
     <h1>About Us</h1>
+
+    <p>
+      Our team is composed of three WPI undergraduate students: Emily Baker, Rui Huang and Akash Shaji.
+      We completed this project in order to fulfill our Interactive Qualifying Project Requirement.
+
+    </p>
+
   </div>
 </template>
+<style lang="scss" scoped>
+
+ p{
+   font-size: 18px;
+   text-align: left;
+   text-indent : 5em;
+ }
+</style>
