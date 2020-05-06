01b94e2e41d4d0753361e9598b160dc22fafde9d
akash <akashshaji321@gmail.com>
2020-05-05 21:49:01 -0400

    reverted change to sus courses, fixed bug in course script and updated coursedata to include all D term courses

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,33 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <div>
-      Hi, We are the Sustainablity Studies group! This is a deliverable for our
-      IQP! We live in an age where the world seems increasingly dismayed in
-      saving the environment. Many don’t seem to understand the urgency of
-      situation we’re in and seem content with what little ‘progress’ we’ve made
-      so far. We need to be taking care of the world for future generations, and
-      as such it's vital that we educate young people on topics relating to
-      sustainability as they stand to lose the most from it. WPI first years are
-      expected to make a 4-year plan that maps their schedules to a set of
-      graduation requirements. Unfortunately, while designing these plans,
-      learning about sustainability is not a factor in their selections. The
-      Climate Crisis, increased poverty and inequalities emphasize how essential
-      a sustainability education is. AASHE explains that “the classroom (is) the
-      true battleground for change,” but WPI students do not realize how many
-      WPI courses cover sustainability, and some WPI faculty lack the resources
-      to integrate sustainability into their curricula. Our project aims to
-      bridge these gaps to make WPI curricula more sustainably focused and to
-      show students what sustainability topics are taught. We interviewed
-      faculty who already teach sustainability related curriculum and surveyed
-      their students to find out what sustainability topics are currently being
-      taught at WPI, and what topics students want to learn about. We used the
-      information gathered through the interviews and surveys in order to
-      generate our deliverable: a website that allows for students to filter
-      through sustainable courses by subject or sustainability topic and for
-      faculty to have easy access to ways sustainability can be integrated in
-      curriculum, potential help for doing so, and the student interest in
-      particular topics or delivery methods.
-    </div>
+    <h1>The Reason</h1>
+    <p>
+      While humanity at large has made great strides in various fields such as medicine, education and life expectancy, this progress is not without great cost to our world.
+      We need to be taking care of the world for future generations, and as such it's vital that we educate young people on topics relating to sustainability because, as a WPI professor explains, “[they] are going to be the ones to deal with it.”
+    </p>
+    <p>
+      WPI first years are expected to make a 4-year plan that maps their schedules to a set of graduation requirements.
+      Unfortunately, while designing these plans, learning about sustainability is not a factor in their selections.
+      The Climate Crisis, increased poverty and inequalities emphasize how essential learning about sustainability is.
+      The Association for the Advancement of Sustainability in Higher Education explains that “the classroom (is) the true battleground for change,” but WPI students do not realize how many courses cover sustainability, and some WPI faculty lack the resources to integrate sustainability into their curricula (“Ten Ways to Integrate Sustainability into the Curriculum,” 2013).
+    </p>
+    <p>
+      Our project aims to promote sustainability in education for WPI faculty and students.
+      We interviewed faculty who already teach sustainability themed curriculum and surveyed their students to find out what sustainability topics are currently being covered at WPI and what topics students want to learn about.
+      We used this information to generate our deliverable: a website that serves both students and faculty. It allows for students to filter through sustainable courses by subject or sustainability topic.
+      It allows faculty to have easy access to ways sustainability can be integrated in curriculum, potential help for doing so, and the student interest in sustainability topics and delivery methods.
+    </p>
+    <p>To read more about our project visit [here] (link will be inserted once our paper is published)</p>
+
+    <h1>About Us</h1>
   </div>
 </template>
