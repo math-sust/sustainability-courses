4e8a946b128efad69ece812d152216a1f2bd6364
akash <akashshaji321@gmail.com>
2020-05-08 11:26:45 -0400

    finished faculty table

--- src/views/Faculty.vue
+++ src/views/Faculty.vue
@@ -30,7 +30,7 @@
           interest in learning about specific Sustainable Development Goals, and
           their desired delivery methods. We surveyed 101 students across 11
           sustainability themed courses and received the following results. To
-          read more about our findings and our methodology, see our paper (in
+          read more about our findings and methodology, see our paper (in
           'About the Project').
           <img src="../assets/studentinterestsdg.jpg" />
           <br /><br />
@@ -46,17 +46,11 @@
               <md-table-head>Description</md-table-head>
             </md-table-row>
             <md-table-row>
-              <md-table-cell>Engineering Unleashed</md-table-cell>
-              <md-table-cell>A website with </md-table-cell>
-            </md-table-row>
-            <md-table-row>
               <md-table-cell
                 ><a
                   href="https://hub.aashe.org/browse/publication/15675/A-Guide-for-Applied-Sustainability-Learning-Projects-Advancing-sustainability-outcomes-on-campus-and-in-the-community"
                 >
-                  A Guide for Applied Sustainability Learning Projects:
-                  Advancing sustainability outcomes on campus and in the
-                  community
+                  A Guide for Applied Sustainability Learning Projects
                 </a></md-table-cell
               >
               <md-table-cell
@@ -65,6 +59,42 @@
                 systematically designed, built and scaled
               </md-table-cell>
             </md-table-row>
+            <md-table-row>
+              <md-table-cell
+              ><a
+                      href="https://www.iise.org/iemagazine/2019-11/html/erdil/erdil.html"
+              >
+                A focus on sustainability as a core value
+              </a></md-table-cell>
+              <md-table-cell>
+                An article that describe links between IE approaches to problem-solving and facility layout
+              </md-table-cell>
+            </md-table-row>
+            <md-table-row>
+
+
+              <md-table-cell>
+                <a href="https://engineeringunleashed.com/">
+                  Engineering Unleashed
+                </a>
+
+              </md-table-cell>
+              <md-table-cell>Engineering Unleashed is a network of thousands of engineering faculty just like you,
+                working to unleash undergraduate engineers so that they can create personal, economic, and societal
+                value through the entrepreneurial mindset.  </md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Development Economics: Theory, Empirical Research, and Policy Analysis </md-table-cell>
+              <md-table-cell>Theory Empirical Research and Policy Analysis by Julie Schaffner offers a means to think
+                about development and policy in a way that is disciplined by economic theory and informed by empirical
+                research. The main goal of this text is to get readers to think rigorously using tools of economic
+                analysis. This text will help to define development objectives, understand the development process,
+                identify potential barriers to development, design candidate development interventions, and analyze the
+                multidimensional impact of theory, empirical research and policy analysis within the study of economic
+                development. </md-table-cell>
+            </md-table-row>
+
           </md-table>
         </md-tab>
       </md-tabs>
